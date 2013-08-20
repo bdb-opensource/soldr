b0870a2c9822df8dec3ea6355ca9e4a2499fbb15
Noam Lewis <lenoam+BDB@gmail.com>
2013-08-20 15:42:01 -0400

    fixup platform configuration and assembly names

--- Cosln.CLI/Properties/AssemblyInfo.cs
+++ Cosln.CLI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Cosln.BuildDependencyTool")]
+[assembly: AssemblyTitle("Cosln.CLI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Cosln.BuildDependencyTool")]
+[assembly: AssemblyProduct("Cosln.CLI")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
